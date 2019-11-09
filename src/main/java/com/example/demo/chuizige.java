74a00d9e201a859a66ee9504397e95f56dab2983
luopeng <492593793@qq.com>
2019-11-09 09:13:50 +0800

    增加动作

---
+++ src/main/java/com/example/demo/chuizige.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: 111
+ * @Date: 2019/11/9 0009 上午 9:05
+ * @Description:
+ */
+@RestController
+public class chuizige {
+    @GetMapping("/addtest")
+    public String uutest() {
+        return "MessgeInfo:just for testing";
+    }
+}
